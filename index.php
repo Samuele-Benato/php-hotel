db3625b18be4642b12b2625bc742104a2c57b059
Samuele-Benato <samuele.benato@gmail.com>
2023-09-26 16:20:24 +0200

    feat : push

--- index.php
+++ index.php
@@ -20,10 +20,10 @@ include './data.php';
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- JS BOOTSTRAP -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"
         defer></script>
     <!-- CSS LINK
     <link rel="stylesheet" href="./css/style.css" /> -->
@@ -51,21 +51,31 @@ include './data.php';
             <tr>
                 <?php foreach ($hotels as $hotel) { ?>
                 <td scope="col">
+                    Name :
+                    <?= $hotel['name']; ?>
+                </td>
+            </tr>
+            <tr>
+                <td scope="col">
+                    Description :
                     <?= $hotel['description']; ?>
                 </td>
             </tr>
             <tr>
                 <td scope="col">
+                    Parking :
                     <?= $hotel['parking']; ?>
                 </td>
             </tr>
             <tr>
                 <td scope="col">
+                    Vote :
                     <?= $hotel['vote']; ?>
                 </td>
             </tr>
             <tr>
                 <td scope="col">
+                    Distance :
                     <?= $hotel['distance_to_center']; ?>
                 </td>
                 <?php }
